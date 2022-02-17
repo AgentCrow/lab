069245e0ba8c2b71b22acd99cec01b236b2a00c4
Paramjeet <oyeparamjeet@gmail.com>
2022-02-17 15:51:04 +0530

    Add Loops into this file

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
 #simple print program
 
 print("Hello World")
+
+
+# a simple a list and loop out everthing that is necessary
+
+Fruits =  ["Apple", "Banana", "Orange"]
+
+
+for i in Fruits:
+    print(i)
